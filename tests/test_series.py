8c960f45bdbaa2f3dd9db791de6b19efb2e6e67e
mhendricks96 <mhendricks96@yahoo.com>
2021-06-08 22:48:08 -0700

    added lucas tests

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,7 +1,7 @@
 from math_series import __version__
 from math_series.series import fibonacci
 
-def test_fib_version():
+def test_version():
     assert __version__ == '0.1.0'
 
 
@@ -13,4 +13,14 @@ def test_fib_four():
 def test_fib_eight():
     actual = fibonacci(8)
     expected = 13
+    assert actual == expected
+
+def test_lucas_four():
+    actual = fibonacci(4)
+    expected = 4
+    assert actual == expected
+
+def test_lucas_eight():
+    actual = fibonacci(8)
+    expected = 29
     assert actual == expected
