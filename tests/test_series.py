781ff845037945ce918f6eca4a5ac52f049600ae
mhendricks96 <mhendricks96@yahoo.com>
2021-06-08 22:44:25 -0700

    added fib tests

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,16 @@
 from math_series import __version__
+from math_series.series import fibonacci
 
-
-def test_version():
+def test_fib_version():
     assert __version__ == '0.1.0'
+
+
+def test_fib_four():
+    actual = fibonacci(4)
+    expected = 2
+    assert actual == expected
+
+def test_fib_eight():
+    actual = fibonacci(8)
+    expected = 13
+    assert actual == expected
