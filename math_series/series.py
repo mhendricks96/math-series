d87c92f385b574cb1bdab418dc37fe5676501557
mhendricks96 <mhendricks96@yahoo.com>
2021-06-08 23:37:21 -0700

    finished tests for sum_series

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,8 @@
 def fibonacci(num):
+  """
+  Every list item should be the sum of the previous 2 items.
+  The list should start with 0 and 1
+  """
   sequence_list = [0, 1]
 
   for i in range (2, num + 1):
@@ -10,6 +14,11 @@ def fibonacci(num):
 
 
 def lucas(num):
+
+  """
+  Every list item should be the sum of the previous 2 items.
+  The list should start with 2 and 1
+  """
   sequence_list = [2, 1]
 
   for i in range (2, num + 1):
@@ -18,3 +27,14 @@ def lucas(num):
     sequence_list.append(next_num)
   
   return sequence_list[num - 1]
+
+
+
+def sum_series(num, start = 0, second = 1):
+  """
+  Every list item should be the sum of the previous 2 list items.
+  If function is called with no parameters, the list should start with 0, and 1.
+  If 2 optional parameters are entered, the list will start with the 2 numbers entered as parameters
+  """
+
+  
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,6 +1,7 @@
 from math_series import __version__
 from math_series.series import fibonacci
 from math_series.series import lucas
+from math_series.series import sum_series
 
 
 def test_version():
@@ -25,4 +26,34 @@ def test_lucas_four():
 def test_lucas_eight():
     actual = lucas(8)
     expected = 29
+    assert actual == expected
+
+def test_sum_series_fib_four():
+    actual = sum_series(4)
+    expected = 2
+    assert actual == expected
+
+def test_sum_series_fib_eight():
+    actual = sum_series(8)
+    expected = 13
+    assert actual == expected
+
+def test_sum_series_lucas_four():
+    actual = sum_series(4, 2, 1)
+    expected = 4
+    assert actual == expected
+
+def test_sum_series_lucas_eight():
+    actual = sum_series(8, 2, 1)
+    expected = 29
+    assert actual == expected
+
+def test_sum_series_evens_four():
+    actual = sum_series(4, 2, 4)
+    expected = 10
+    assert actual == expected
+
+def test_sum_series_evens_eight():
+    actual = sum_series(8, 2, 4)
+    expected == 68
     assert actual == expected
