5c45ad9bd71b9a0a73719bafce469016e10f58d7
mhendricks96 <mhendricks96@yahoo.com>
2021-06-08 23:07:50 -0700

    finished fibonacci function

---
+++ math_series/series.py
@@ -0,0 +1,13 @@
+def fibonacci(num):
+  sequence_list = [0, 1]
+
+  for i in range (2, num + 1):
+    next_num = sequence_list[-1] + sequence_list[-2]
+
+    sequence_list.append(next_num)
+  
+  return sequence_list[num - 1]
+
+
+def lucas():
+  print("mike")
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,7 @@
 from math_series import __version__
 from math_series.series import fibonacci
+from math_series.series import lucas
+
 
 def test_version():
     assert __version__ == '0.1.0'
